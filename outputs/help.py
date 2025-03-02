0805664be2f1431d343e6e50819ffefe6731853b
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-03-02 14:19:09 +0000

    feat: Setup (#1)
    - Setup embeds
    - Configuration files (to prevent repetitive code)
    - Help command

---
+++ outputs/help.py
@@ -0,0 +1,14 @@
+""" 
+    String configuration files
+
+    String classes: 
+"""
+
+FUN_COMMANDS_LIST: list = [
+    "> ping",
+    "pong"
+]
+
+COMMANDS_LIST: dict = {
+    "fun" : "\n> ".join(FUN_COMMANDS_LIST)
+}
