1ba62874678b2bf89da87f0d52e9c0b6fe7c0b7f
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-03-19 22:40:15 +0000

    chore: Testing new structure (#12)
    - ensuring that new changes are being made to production bot

--- prod/main.py
+++ prod/main.py
@@ -43,7 +43,7 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("I have no idea what you're talking about :blush:")
+        await ctx.send("I have no idea what you're talking about! :blush:")
 
 @bot.command()
 async def help(ctx, *, mes=None):
