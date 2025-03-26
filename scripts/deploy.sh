8e263c1ebdd9d76770d9a643d3cbd1d2ef358eb9
RJ <rjparaiso369@gmail.com>
2025-03-26 23:02:44 +0000

    fix: Fixing deployment issues

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### This is the deployment script for deploying the bot in the server
+
+cd
+source venv/bin/activate
+cd JaneBot/
+pip install -r requirements.txt
+cd prod
+git pull origin main
+pkill -f "python3 main.py" || true
+nohup python3 main.py > bot.log 2>&1 &
+echo "Deployment complete!
