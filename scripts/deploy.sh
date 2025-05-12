42a892d36006deea56cb4b41b12c47f1d56538f5
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-05-12 23:52:47 +0100

    fix(JB-44): Fixing deploy script (#32)
    - Fixing the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,6 @@ cd
 source venv/bin/activate
 
 # Change to main directory
-cd JaneBot 
-git pull origin main
-pip install -r requirements.txt
-cd /code/prod/JaneBot
-pkill -f "python3 main.py" || true
-nohup python3 main.py > bot.log 2>&1 &
+cd JaneBot && git pull origin main && pip install -r requirements.txt
+cd ~/JaneBot/code/prod/JaneBot && pkill -f "python3 main.py" || true && nohup python3 main.py > bot.log 2>&1
 echo "Deployment complete!"
