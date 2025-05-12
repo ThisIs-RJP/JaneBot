40638c0190038b7182c25cc66506e23781bb4eec
RJ <rjparaiso369@gmail.com>
2025-05-13 00:03:01 +0100

    fix(JB-44): Triggering the pipelines

--- code/prod/JaneBot/main.py
+++ code/prod/JaneBot/main.py
@@ -15,7 +15,6 @@ from discord.ext          import commands, tasks
 from discord.ext.commands import has_permissions, CheckFailure
 from dotenv               import load_dotenv, find_dotenv 
 
-
 ## String Configuration Files
 from outputs.admin  import *
 from outputs.embeds import *
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,5 @@ source venv/bin/activate
 
 # Change to main directory
 cd JaneBot && git pull origin main && pip install -r requirements.txt
-cd ~/JaneBot/code/prod/JaneBot && pkill -f "python3 main.py" || true && nohup python3 main.py > bot.log 2>&1
+cd ~/JaneBot/code/prod/JaneBot && pkill -f "python3 main.py" || true && nohup python3 main.py > bot.log 2>&1 &
 echo "Deployment complete!"
